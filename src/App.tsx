bffcb29e25bad2d6cc6a5ca5105690857fdab7e0
Josh Weaver <joshweaver23@gmail.com>
2024-06-17 11:06:24 -0400

    level interview

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,104 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
+interface Stops {
+  [key: string]: number[]
+}
+
+interface Station {
+  id: string
+  location: number[]
+  name: string
+  stops: Stops
+}
+
+interface ArrivalTime {
+  route: string
+  time: string
+}
+interface ArrivalTimes {
+  N?: ArrivalTime[]
+  S?: ArrivalTime[]
+}
+
 function App() {
-  return <h1 className="text-3xl text-center">Hello world!</h1>
+  const [stations, setStations] = useState<Station[]>([])
+  const [selectedStation, setSelectedStation] = useState<string | null>(null)
+  const [selectedArrivalTimes, setSelectedArrivalTimes] =
+    useState<ArrivalTimes>({})
+
+  useEffect(() => {
+    if (stations.length === 0) {
+      try {
+        fetch(
+          'https://raw.githubusercontent.com/jonthornton/MTAPI/master/data/stations.json',
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            let stationArr = []
+            for (const key in data) {
+              stationArr.push(data[key])
+            }
+
+            setStations(stationArr)
+          })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }, [])
+
+  const handler = (id: string) => {
+    return () => {
+      setSelectedStation(id)
+      try {
+        fetch(
+          `https://cors-anywhere.herokuapp.com/https://api.wheresthefuckingtrain.com/by-id/${id}`,
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            setSelectedArrivalTimes(data.data[0])
+          })
+        console.log(selectedArrivalTimes)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Stations</h1>
+        <ul>
+          {stations.map(({ id, name }) => (
+            <li key={id}>
+              <h3>{name}</h3>
+              <button onClick={handler(id)}>Get Arrival Times</button>
+              {id === selectedStation && (
+                <div>
+                  <h3>Northbound</h3>
+                  <ul>
+                    {selectedArrivalTimes.N &&
+                      selectedArrivalTimes['N'].map(({ route, time }) => (
+                        <li>{`route: ${route}, arrival time: ${time}`}</li>
+                      ))}
+                  </ul>
+                  <h3>Southbound</h3>
+                  <ul>
+                    {selectedArrivalTimes.S &&
+                      selectedArrivalTimes['S'].map(({ route, time }) => (
+                        <li>{`route: ${route}, arrival time: ${time}`}</li>
+                      ))}
+                  </ul>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  )
 }
 
 export default App
